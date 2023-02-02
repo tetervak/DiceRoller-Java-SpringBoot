16b43f23721ae640096e4007f63121ce9a0c9934
Alexander Tetervak <tetervak@sheridancollege.ca>
2023-02-02 14:01:52 -0500

    added the test for cookieDataService2

---
+++ src/test/java/ca/tetervak/diceroller/cookiedata/CookieDataService2Test.java
@@ -0,0 +1,32 @@
+package ca.tetervak.diceroller.cookiedata;
+
+import ca.tetervak.diceroller.model.RollData;
+import ca.tetervak.diceroller.model.RollDataImpl;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static java.lang.System.out;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CookieDataService2Test {
+
+    List<Integer> inputList = List.of(2, 6, 12, 1, 107, 1);
+    CookieDataService service = new CookieDataService2();
+
+    @Test
+    void encodeRollData() {
+        out.println(inputList);
+        out.println(service.encodeRollData(new RollDataImpl(inputList)));
+    }
+
+    @Test
+    void decodeRollData() {
+        out.println(inputList);
+        String cookieData = service.encodeRollData(new RollDataImpl(inputList));
+        RollData rollData = service.decodeRollData(cookieData);
+        out.println(rollData.getValues());
+        assertEquals(inputList, rollData.getValues());
+    }
+
+}
