033a9cae56d9084c5da1db38d6f8e8e3ddd1fafc
Alex Tetervak <tetervak@sheridancollege.ca>
2023-05-28 23:16:27 -0400

    used streams in the cookie service

--- src/main/java/ca/tetervak/diceroller/cookiedata/CookieDataServiceImpl.java
+++ src/main/java/ca/tetervak/diceroller/cookiedata/CookieDataServiceImpl.java
@@ -5,27 +5,24 @@ import ca.tetervak.diceroller.model.RollDataImpl;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CookieDataServiceImpl implements CookieDataService{
     @Override
     public String encodeRollData(RollData rollData) {
-        List<Integer> values = rollData.getValues();
-        StringBuilder builder = new StringBuilder(values.size());
-        for(int value: values){
-            builder.append(value);
-        }
-        return builder.toString();
+        return rollData.getValues().stream()
+                .map(Object::toString)
+                .collect(Collectors.joining("+"));
     }
 
     @Override
     public RollData decodeRollData(String cookieValue) {
-        int length = cookieValue.length();
-        List<Integer> values = new ArrayList<>(length);
-        for(int i = 0; i < length; i++){
-            values.add(Integer.parseInt(String.valueOf(cookieValue.charAt(i))));
-        }
+        List<Integer> values = Arrays.stream(cookieValue.split("\\+"))
+                        .map(Integer::parseInt)
+                        .toList();
         return new RollDataImpl(values);
     }
 }
